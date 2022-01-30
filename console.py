7114ca4d2e5d52f3696c5be59475dff42b22277d
Evans262 <evancechapuma62@gmail.com>
2022-01-31 00:27:44 +0200

    fixed bugs on console

--- console.py
+++ console.py
@@ -9,17 +9,17 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
+# from models.amenity import Amenity
 from models.place import Place
-from models.review import Review
+# from models.review import Review
 from models.user import User
 from itertools import count
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    my_class = {"BaseModel": BaseModel, "Amenity": Amenity,
-                "City": City, "Place": Place, "Review": Review,
+    my_class = {"BaseModel": BaseModel,
+                "City": City, "Place": Place,
                 "User": User, "State": State}
     file = None
 
