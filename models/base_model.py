8509a6b8f6462a476e29b7b4a704e97cc3240fa3
Evans262 <evancechapuma62@gmail.com>
2022-01-31 00:12:43 +0200

    fixed some bugs

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""BaseModel class"""
+"""BaseModel class Module"""
 
 
 from uuid import uuid4
@@ -7,13 +7,14 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Base object for all common attributes:
-    attr1: id
-    attr: created_at
-    attr3: updated_at"""
-
+    """ A class called BaseModel
+    attributes:
+    att1: id
+    att2: created_at
+    att3: updated_at
+    """
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes"""
+        """initializing an instance"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -23,8 +24,7 @@ class BaseModel:
                     if (key == "created_at" or key == "updated_at"):
                         if (type(value) == str):
                             new_object = datetime.strptime(
-                                value, '%Y-%m-%dT%H:%M:%S.%f'
-                            )
+                                value, '%Y-%m-%dT%H:%M:%S.%f')
                             self.__dict__[key] = new_object
                     else:
                         self.__dict__[key] = value
@@ -35,19 +35,22 @@ class BaseModel:
                                      self.__dict__)
 
     def save(self):
-        """Updated public instance attribute
-        updated_at with the current datetime"""
-        from models import storage
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
+        from models import storage
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all
-        keys/values of __dict__ of the instance"""
-        updated_dict = self.__dict__.copy()
-        updated_dict['__class__'] = self.__class__.__name__
-        updated_dict['created_at'] = self.created_at.strftime(
+        """returns a dictionary containing
+        all keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.strftime(
             "%Y-%m-%dT%H:%M:%S.%f")
-        updated_dict['updated_at'] = self.updated_at.strftime(
+        new_dict['updated_at'] = self.updated_at.strftime(
             "%Y-%m-%dT%H:%M:%S.%f")
-        return updated_dict
+        return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,31 +3,34 @@ from datetime import datetime
 from models.base_model import BaseModel
 from models.state import State
 from models.city import City
+# from models.amenity import Amenity
 from models.place import Place
+# from models.review import Review
 from models.user import User
 import json
 import os
 
+
 class FileStorage:
-    """A class wit the attributes:
-    attri1(__file_path): JSON file
-    attr2(__objects): dictionary
-    """
+    """ A class called FileStorage
+    attributes:
+    attr1(__file_path): JSON file path
+    attr2(__objects): dictionary"""
     __file_path = "file.json"
     open(__file_path, "a")
     __objects = {}
-    
+
     def all(self):
-        """Returns a dictionary __objects"""
+        """ return all objects"""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.__dict__ + '.' + obj.id
+        """ sets in __objects the obj with key"""
+        key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        """Selializes __objects to JSON file"""
+        """ save to JSON"""
         dict = {}
         for key, value in self.__objects.items():
             dict[key] = value.to_dict()
@@ -35,16 +38,14 @@ class FileStorage:
             json.dump(dict, my_file)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects
-        (only if the __file_path exists)
-        """
+        """reload from JSON"""
         my_class = {"BaseModel": BaseModel,
                     "City": City, "Place": Place,
                     "User": User, "State": State}
-        dic_to_fill = {}
+        dict_to_fill = {}
         if(os.stat(self.__file_path).st_size != 0):
             with open(self.__file_path) as json_file:
                 dict_to_fill = json.load(json_file)
-                for key, value in dic_to_fill.items():
-                    self.__objects[key] = my_class[value['__class__']](**value)
+                for key, value in dict_to_fill.items():
+                    self.__objects[key] =\
+                        my_class[value['__class__']](**value)
