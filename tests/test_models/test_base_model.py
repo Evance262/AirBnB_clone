71ceae1f93acd7c6e2133fb554d60b8bb2de526a
Evans262 <evancechapuma62@gmail.com>
2022-01-30 17:51:28 +0200

    changed permissions

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -18,19 +18,33 @@ class TestBaseModel(unittest.TestCase):
                         "Found code style errors (and warnings).")
 
     def test_id(self):
-        pass
+        new = BaseModel()
+        self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        pass
+        new = BaseModel()
+        self.assertEqual(type(new.created_at),
+                         datetime.datetime)
 
     def test_updated_at(self):
-        pass
+        new = BaseModel()
+        self.assertEqual(type(new.updated_at),
+                            datetime.datetime)
+
+    def test_str(self):
+        new = BaseModel()
+        self.assertEqual(str(new), "[{:s}] ({:s}) {}".format(
+            new.__class__.__name__, new.id, new.__dict__))
 
     def test_save(self):
-        pass
+        base = BaseModel()
+        base.save()
+        self.assertTrue(os.path.exists(self.path_file('file.json')))
 
         def to_dict(self):
-            pass
+            base = BaseModel()
+            self.assertEqual(type(base.to_dict()), dict)
+
 
 if __name__ == '__main__':
     unittest.main()
